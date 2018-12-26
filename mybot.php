76e872d51101c682517693f1e101a9fb9c4ee62f
Freedomhamid <46088467+Freedomhamid@users.noreply.github.com>
2018-12-27 02:31:34 +0330

    Add files via upload

---
+++ mybot.php
@@ -0,0 +1,13 @@
+<?php
+$bot_token ="764811448:AAEKd1POpU6bzuCRGCeFAbzpGd2uddFnnIM";
+$website ="https://api.telegram.org/bot".$bot_token;
+$update= file_get_contents('php://input');
+$updateArray= json_decode($update,true);
+$chatid = $updateArray["message"] ["chat"] ["id"];
+$text = $updateArray["message"] ["text"];
+$username=$updateArray["message"] ["chat"] ["username"];
+$first_name=$updateArray["message"] ["chat"] ["first_name"];
+$lastname=$updateArray["message"] ["chat"] ["last_name"];
+&url=$website."/sendmessage?chat_id=".$chatid."&text=haha";
+file_get_contents($url);
+?>
