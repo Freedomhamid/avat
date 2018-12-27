adf018a29d378f43f8e1a54215ece243e041031d
Freedomhamid <46088467+Freedomhamid@users.noreply.github.com>
2018-12-27 03:31:04 +0330

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -5,9 +5,9 @@ $update = file_get_contents('php://input');
 $updateArray = json_decode($update,true);
 $chatid = $updateArray["message"] ["chat"] ["id"];
 $text = $updateArray["message"] ["text"];
-$username = $updateArray["message"] ["chat"] ["username"];
-$first_name = $updateArray["message"] ["chat"] ["first_name"];
+$username=$updateArray["message"] ["chat"] ["username"];
+$first_name=$updateArray["message"] ["chat"] ["first_name"];
 $lastname=$updateArray["message"] ["chat"] ["last_name"];
-$url = $website."/sendmessage?chat_id=".$chatid."&text=haha";
+$url=$website."/sendmessage?chat_id=".$chatid."&text=haha";
 file_get_contents($url);
 ?>
